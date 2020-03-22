c120e8aff6157a929e26b4a8c57472da07076b65
LorenzoFramba <lorenzoframba-1@studenti.unitn.it>
2020-03-22 13:09:43 +0100

    paypal

--- online.py
+++ online.py
@@ -43,6 +43,33 @@ if st.checkbox('Guarda i Dati'):
     st.write(df)
     st.write(df_prov)
 
+st.sidebar.markdown(
+    ("Tutti i codici Open Source possono essere trovati qua  ")
+    + "[GitHub](https://github.com/LorenzoFramba/CoronaVirus_in_Italy)."
+)
+
+
+st.sidebar.markdown(
+    ("Questo sito è frutto di lavoro individuale e autosostenuto. Se hai la possibilità di fare una donazione, lo apprezzerei tantissimo!  ")
+    + "[PayPal](paypal.me/rappasta)."
+)
+
+
+st.success(
+    ("Questo sito è frutto di lavoro individuale e autosostenuto. Se hai la possibilità di fare una donazione, lo apprezzerei tantissimo!  ")
+    + "[PayPal](paypal.me/rappasta)."
+)
+
+
+st.markdown(
+    (  "Tutti i dati visualizzati in questo sito provengono dal Ministero della Salute, elaborati dal Dipartimento di Protezione Civile. "
+        "I loro dati sono visualizzabili [COVID-19 Italia - Monitoraggio situazione](https://github.com/pcm-dpc/COVID-19) su licenza "
+        "[CC BY 4.0](https://creativecommons.org/licenses/by/4.0/). "
+        "I DATI SONO SEMPRE AGGIORNATI IN TEMPO REALE ALLE 18 DI OGNI GIORNO. "
+    )
+)
+
+
 regioni=utils.getLocations(df, True)
 province=utils.getLocations(df_prov, False)
 
@@ -91,15 +118,18 @@ inizio = datetime.date(2020, 2, 24)
 #data_scelta = st.slider(f"Seleziona giornata da analizzare tra {today} e {inizio}", inizio, today, today)
 
 
+st.success(
+    ("Questo sito è frutto di lavoro individuale e autosostenuto. Se hai la possibilità di fare una donazione, lo apprezzerei tantissimo!  ")
+    + "[PayPal](paypal.me/rappasta)."
+)
 
 
-
-chosen_date = st.date_input(f"Seleziona giornata da analizzare tra {inizio} e {today} (non compreso) ", today)
+chosen_date = st.date_input(f"Seleziona giornata da analizzare tra {inizio} e {today}", today)
 if  chosen_date > inizio  and chosen_date <= (today):
     filtered_data = df_prov[df_prov["data"] == chosen_date]
     df,prov = utils.filtra(filtered_data)
     prov=prov.reset_index()
-    province_da_visualizzare = st.slider('Province piú infette da Visualizzare', 3, 10, 5)
+    province_da_visualizzare = st.slider('Province piú infette da Visualizzare', 3, 15, 5)
     
     st.altair_chart(utils.altair_chart(prov.head(province_da_visualizzare), "Provincia", "Casi" ))
     
@@ -107,7 +137,7 @@ if  chosen_date > inizio  and chosen_date <= (today):
     #st.dataframe(prov[:province_da_visualizzare])
     st.pydeck_chart(utils.crea_mappa(df))
 else:
-    st.error( f"Selezionare un giorno compreso tra {inizio} e {today}  (non compreso) ")
+    st.error( f"Selezionare un giorno compreso tra {inizio} e {today} ")
 
 
 
